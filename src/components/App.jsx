977b6e43e76cfa6727f6c97b9ac82632121d4e79
Sam Goldsmith <samggold@gmail.com>
2019-07-03 10:25:02 -0600

    Reformats base compononent to start project idea

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <main>
+    <h1>Find Top Foodie Stories in Your City</h1>
+  </main>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import HelloWorld from 'Components/HelloWorld';
+import App from 'Components/App';
+import 'materialize-css/dist/css/materialize.min.css';
 import './css/main.scss';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <HelloWorld/>,
+  <App/>,
   rootElement
 );
