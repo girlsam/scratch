1d8980fa4b2e4d564b3da60630c02d1809e1c3a8
Sam Goldsmith <samggold@gmail.com>
2019-07-08 13:05:01 -0600

    More updates to scaffolding but still pretty ugly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import ArticleList from 'Components/articles/ArticleList';
 import Navigation from 'Components/navigation';
 
 import collections from '../api/collections.json';
 
-const App = () => (
-  <Fragment>
-    <nav>
-      <Navigation />
-    </nav>
-    <main>
-      <h1>Find Top Foodie Stories in Your City</h1>
-      <ArticleList articles={ collections } />
-    </main>
-  </Fragment>
-);
+const flattenedCollections = () => [...(collections.collections).map(collection => collection.collection)];
+
+const App = () => {
+  return (
+    <>
+      <nav>
+        <Navigation />
+      </nav>
+      <main>
+        <h1>Find Top Foodie Stories in Your City</h1>
+        <ArticleList articles={ flattenedCollections() } />
+      </main>
+    </>
+  );
+}
 
 export default App;
